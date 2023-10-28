f51d85110f9fb60189cd098d11b0c4acce1a296f
Miguel Ferreira Cordero <72079312+ferreiracorderomiguel@users.noreply.github.com>
2023-10-28 20:03:07 +0200

    If there is no WiFi connection gives an error on screen

--- ESP8266-FechaHoraPantalla.ino
+++ ESP8266-FechaHoraPantalla.ino
@@ -8,42 +8,61 @@
 #include <Timezone.h>
 
 // Configura WiFi
-const char* ssid = "mitiaritalacantaora";
-const char* password = "Elvis2020";
+const char* ssid = "xxxxxx";
+const char* password = "xxxxxx";
 
 // Configura NTP
 const long gmtOffset_sec = 3600;
-const int daylightOffset_sec = 0;  // No hay horario de verano en España
+const int daylightOffset_sec = 0;
 
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "europe.pool.ntp.org", gmtOffset_sec, daylightOffset_sec);
 
-TimeChangeRule CEST = {"CEST", Last, Sun, Mar, 2, 120};  // Regla para horario de verano
-TimeChangeRule CET = {"CET", Last, Sun, Oct, 3, 0};     // Regla para horario estándar
+TimeChangeRule CEST = {"CEST", Last, Sun, Mar, 2, 120};
+TimeChangeRule CET = {"CET", Last, Sun, Oct, 3, 0};
 Timezone EuropeMadrid(CEST, CET);
 
 U8G2_SSD1306_128X64_NONAME_F_SW_I2C u8g2(U8G2_R0, 14, 12, U8X8_PIN_NONE);
 
 int prevSecond = -1;
-bool timeInitialized = false;  // Variable para verificar si la hora se ha inicializado
+bool timeInitialized = false;
+bool wifiConnected = false;
 
 void setup() {
   u8g2.begin();
   u8g2.clearBuffer();
-  u8g2.setFont(u8g2_font_ncenB08_tr); // Configura un tamaño de fuente adecuado
+  u8g2.setFont(u8g2_font_ncenB08_tr);
 
   Serial.begin(115200);
   Serial.println("");
   Serial.print("Conectando a ");
   Serial.print(ssid);
   WiFi.begin(ssid, password);
+
+  displayInitialMessage(false);
+
+  unsigned long startTime = millis();
+  bool errorDisplayed = false;
+
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
+    
+    if (millis() - startTime >= 10000 && !errorDisplayed) {
+      // Si han pasado 10 segundos y no se ha conectado, muestra un mensaje de error
+      displayInitialMessage(true);
+      errorDisplayed = true;
+    }
   }
+
+  if (errorDisplayed) {
+    return; // Salir del setup si se ha mostrado un error
+  }
+
   Serial.println("");
   Serial.print("Conectado a WiFi con dirección IP: ");
   Serial.println(WiFi.localIP());
+  wifiConnected = true;
 
   // Muestra la información del WiFi y la dirección IP durante 5 segundos
   displayWiFiInfo(5000);
@@ -77,20 +96,42 @@ void loop() {
   delay(1000);
 }
 
+void displayInitialMessage(bool error) {
+  u8g2.clearBuffer();
+  u8g2.setFont(u8g2_font_ncenB08_tr);
+  u8g2.drawStr(0, 10, "Connecting to Wi-Fi...");
+  
+  if (error) {
+    // Se muestra este texto si no se ha logrado la conexión a internet.
+    u8g2.drawStr(0, 26, "Error connecting to");
+    u8g2.drawStr(0, 35, "Wi-Fi. Please make");
+    u8g2.drawStr(0, 44, "sure that this device");
+    u8g2.drawStr(0, 53, "is in range and Wi-Fi");
+    u8g2.drawStr(0, 62, "data is correct.");
+  }
+  u8g2.sendBuffer();
+}
+
 void displayWiFiInfo(unsigned long duration) {
   u8g2.clearBuffer();
 
   u8g2.setFont(u8g2_font_ncenB08_tr);
   u8g2.drawStr(0, 10, ssid);
 
-  u8g2.setFont(u8g2_font_ncenB08_tr);
   const char* textIpAdd = "IP Address:";
-  int dirX = (128 - u8g2.getStrWidth(textIpAdd)) / 2;
-  u8g2.drawStr(dirX, 40, textIpAdd);
-
+  int dirXIp = (128 - u8g2.getStrWidth(textIpAdd)) / 2;
+  u8g2.drawStr(dirXIp, 25, textIpAdd);
   String ipAddress = WiFi.localIP().toString();
   int ipX = (128 - u8g2.getStrWidth(ipAddress.c_str())) / 2;
-  u8g2.drawStr(ipX, 55, ipAddress.c_str());
+  u8g2.drawStr(ipX, 35, ipAddress.c_str());
+
+  const char* textSignal = "WIFI strength:";
+  int dirXSignal = (128 - u8g2.getStrWidth(textSignal)) / 2;
+  u8g2.drawStr(dirXSignal, 50, textSignal);
+  int signalStrength = WiFi.RSSI();
+  String signalStrengthStr = String(signalStrength) + " dBm";
+  int signalX = (128 - u8g2.getStrWidth(signalStrengthStr.c_str())) / 2;
+  u8g2.drawStr(signalX, 60, signalStrengthStr.c_str());
 
   u8g2.sendBuffer();
   delay(duration);
